4277fbb6292d066ca9c1fca75ed38db6b2c32c13
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-10-15 17:57:39 +0300

    v1

---
+++ find-obeb/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace find_obeb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 21, b = 35;
+            Console.WriteLine(findOBEB(a, b));
+        }
+
+        private static int findOBEB(int a, int b)
+        {
+            if (a < b)
+            {
+                replace(ref a, ref b);
+            }
+            int r;
+            while (b != 0)
+            {
+                r = a % b;
+                a = b;
+                b = r;  
+            }
+            return a;
+        }
+
+        private static void replace(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
